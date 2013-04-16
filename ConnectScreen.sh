bb569800c0c0c63f8e1720b08a245ecd2c918984
benkrikler <mr.krikler@googlemail.com>
2013-04-16 18:51:43 +0100

    Tidied the output and the code for ConnectScreen.sh

--- ConnectScreen.sh
+++ ConnectScreen.sh
@@ -1,18 +1,36 @@
 #! /bin/bash
 
+#Get ID's for all running screen sessions:
 sessions=($(screen -ls|grep --color=never -G ')$'| sed -e 's/^[ \t]*//' -e 's/[ \t](.*)$/ /' |tr '\n' ' '))
+
+#If there's only one session, reconnect to it:
 if [ ${#sessions[@]} -lt 2 ];then
     screen -d -R
     exit 0
 fi
 
+#If no command line arguments then request screen session interactively:
 if [ $# -lt 1 ]; then
-    screen -ls
+    i=1
+    # This loop just prints the information prettily:
+    screen -ls |while read line; do
+	echo $line|grep ')$'&>/dev/null
+	if [ $? -eq 0 ];then
+	    echo -e "  [$i] \t $line"
+	    i=$((i+1))
+	elif [[ "$line" != "\r\n" ]];then
+	    echo "$line" 
+	fi
+    done
+    echo -n "Choose which session you want (1-${#sessions[@]}): "
+    #Read value from stdin
     read SELECTED
 else 
+# Else session is given as an argument to this command
     SELECTED=$1
 fi
-echo "${sessions[@]}"
+
+#Check everything is ok and connect to selected screen
 if [[ "$SELECTED" =~ ^[^0-9]*$ ]]; then
     echo "You can only give me a number"
 elif [ -z "${sessions}" ] ; then
